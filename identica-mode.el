05d1da6f58511120c0145ac5af09a42db74e3dff
Gabriel Saldana <gabriel@gabrielsaldana.org>
2011-02-20 23:25:38 -0600

    Added ability to expand a short URL in buffer.
    Still not perfect, needs to insert a clickable url but its a basic start.
    
    HTTP Header retrieval code based from http-headers.el at: http://members.fortunecity.com/salkosuo/emacs/http-headers.html

--- identica-mode.el
+++ identica-mode.el
@@ -19,7 +19,7 @@
 ;;     Sean Neakums (patches of bugs flagged by byte-compiler)
 ;;     Shyam Karanatt <shyam@swathanthran.in> (several patches and code cleanup, new http backend based on url.el)
 ;;     Tezcatl Franco <tzk@riseup.net> (ur1.ca support)
-;;     Anthony Garcia <lagg@lavabit.com> (fix for icon-mode)
+;http://www.pcworld.com/businesscenter/article/219481/why_you_need_to_have_a_linux_livecd.htmla <lagg@lavabit.com> (fix for icon-mode)
 
 ;; Identica Mode is a major mode to check friends timeline, and update your
 ;; status on Emacs.
@@ -35,7 +35,7 @@
 
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; MERCHANTABILITY or FITNESS FORCouldn't findSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
@@ -1429,6 +1429,78 @@ this dictionary, only if identica-urlshortening-service is 'google.
 	    (narrow-to-region (car url-bounds) (cdr url-bounds))
 	    (delete-region (point-min) (point-max))
 	    (insert url)))))))
+
+(defun identica-expand-replace-at-point ()
+  "Replace the url at point with a tiny version."
+  (interactive)
+  (let ((url-bounds (bounds-of-thing-at-point 'url)))
+    (when url-bounds
+      (let ((uri (identica-expand-shorturl (thing-at-point 'url))))
+	(when uri
+	  (set-buffer (get-buffer identica-buffer))
+	  (save-restriction
+	    (setq buffer-read-only nil)
+	    (narrow-to-region (car url-bounds) (cdr url-bounds))
+	    (delete-region (point-min) (point-max))
+	    (insert uri)
+	    (setq buffer-read-only t))))))
+
+(defun identica-expand-shorturl (url)
+  "Return the redirected url, or nil if not found"
+  (setq temp-buf (get-buffer-create "*HTTP headers*"))
+  (set-buffer temp-buf)
+  (erase-buffer)
+  (goto-char 0)
+  (setq url (replace-regexp-in-string "http://" "" url))
+  (setq host (substring url 0 (string-match "/" url)))
+  (if (string-match "/" url)
+      (setq file (substring url (string-match "/" url)))
+    (setq file "/"))
+  (setq tcp-connection
+	(open-network-stream
+	 "Identica URLExpand"
+	 temp-buf
+	 host
+	 80))
+    (set-marker (process-mark tcp-connection) (point-min))
+    (set-process-sentinel tcp-connection 'identica-http-headers-sentinel)
+    (setq request (concat "GET http://" url " HTTP/1.1\r\n"
+			  "Host:" host "\r\n"
+			  "User-Agent: " (identica-user-agent) "\r\n"
+			  "Authorization: None\r\n"
+			  "Accept-Charset: utf-8;q=0.7,*;q=0.7\r\n\r\n"))
+    (process-send-string tcp-connection request)
+    (setq tmp (concat "http://" host file))
+    (sit-for 2)
+    (setq location (identica-expand-headers-parse tmp tcp-connection))
+    (delete-process tcp-connection)
+    (kill-buffer temp-buf)
+    location)
+
+(defun identica-http-headers-sentinel (process string)
+  "Process the results from the efine network connection.
+process - The process object that is being notified.
+string - The string that describes the notification."
+
+  )
+
+(defun identica-expand-headers-parse (url process)
+  "Parse header"
+  (let (
+	(buffer)
+	(headers)
+	(header-end)
+	)
+    (setq buffer (get-buffer-create "*HTTP headers*"))
+    (set-buffer buffer)
+    (goto-char 0)
+    (setq location
+	  (if (search-forward-regexp "^Location: \\(http://.*?\\)\r?$" nil t)
+	      (match-string-no-properties 1)
+	    url))
+    (setq location (replace-regexp-in-string "\r" "" location))
+    location))
+
 ;;;
 ;;; Commands
 ;;;
