bfe17d0cacbdf44adb11ccac59e181e3922a7045
Gabriel Saldana <gabriel@justia.com>
2011-06-15 10:23:23 -0500

    Adds extra LF to default string format

--- identica-mode.el
+++ identica-mode.el
@@ -293,7 +293,7 @@ The available choices are:
   :type 'string
   :group 'identica-mode)
 
-(defcustom identica-status-format "%i %s,  %@:\n  %t // from %f%L%r\n"
+(defcustom identica-status-format "%i %s,  %@:\n  %t // from %f%L%r\n\n"
   "The format used to display the status updates"
   :type 'string
   :group 'identica-mode)
